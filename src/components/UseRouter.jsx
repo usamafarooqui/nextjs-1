e789f001239b5af5b3503f186ba29daeccdcffa3
usamafarooqui <muhammadusamafarooqui@gmail.com>
2024-03-13 00:31:35 +0500

    css added

--- src/components/UseRouter.jsx
+++ src/components/UseRouter.jsx
@@ -1,15 +1,14 @@
 "use client"
 
-import React from 'react'
 import { useRouter } from 'next/navigation'
-
+import styles from '../app/css/learnCss.module.css'
 const UseRouter = () => {
     const router = useRouter();
     console.log(router)
   return (
     <div>UseRouter
     
-    <h1>hello</h1>
+    <h1 className={styles.mycolor}>hello</h1>
     <button onClick={()=> router.push('/admin')}>Click me</button>
     </div>
   )
